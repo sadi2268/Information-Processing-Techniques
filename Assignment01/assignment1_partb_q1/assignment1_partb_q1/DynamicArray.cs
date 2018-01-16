945ae7d9801aa18bf897de6cfe70885cbcc10095
Syed Sadiq Ali Naqvi <syedsadiqalinaqvi@gmail.com>
2018-01-16 15:15:50 +0500

    Add solution to Part B

---
+++ Assignment01/assignment1_partb_q1/assignment1_partb_q1/DynamicArray.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace assignment1_partb_q1
+{
+    /*
+     *Following class is the net result of Part B ques 1 AND 2. 
+     * 
+     * Part B Ques 1 required us to change the DynamicIntArray to DynamicArray. This involves just 
+     * changing the 'int' datatype to 'T' among other small changes.
+     * 
+     * Part B Ques 2 was the REAL CHALLENGE as it involved exploring/implementing different functions
+     * of the IList<T> interface.
+     * 
+     */
+    class DynamicArray<T> : IList<T>
+    {
+        public int size;
+        public int capacity;
+        public T[] dy_arr;
+
+        public DynamicArray()
+        {
+            capacity = 10;
+            dy_arr = new T[capacity];
+            size = 0;
+        }
+
+        public DynamicArray(int cap)
+        {
+            capacity = cap;
+            dy_arr = new T[capacity];
+            size = 0;
+        }
+
+        //lambda expression used => for anonymous functions
+        public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        //public int Count = size; //size has to be static for this to work. Which is not what we desire.
+        public int Count { get => size; }
+
+        public bool IsReadOnly => false;
+
+        public void Add(T element)
+        {
+            if (size < capacity - 5)
+            {
+                dy_arr[size] = element;
+                size++;
+            }
+            else
+            {
+                //Console.WriteLine("Resizing!");
+                Array.Resize(ref dy_arr, capacity * 2);
+                capacity = capacity * 2;
+                dy_arr[size] = element;
+                size++;
+            }
+        }
+
+        public void Clear()
+        {
+            //Using Array.Clear Method (Array, Int32, Int32) : Sets a range of elements in an array to the default value of each element type.
+            //Trivially we could also traverse through the array and make every value to 0.
+            Array.Clear(dy_arr, 0, size);
+            size = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            int index = Array.IndexOf(dy_arr, item);
+            if (index == -1)
+                return false;
+            else
+                return true;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            for (int newIndex = 0; arrayIndex < size; arrayIndex++)
+            {
+                array[newIndex] = dy_arr[arrayIndex];
+                newIndex++;
+            }
+        }
+
+        public T Get(int index)
+        {
+            if (index < size)
+            {
+                return dy_arr[index];
+            }
+            else
+                //Since the return type is T, we have to cast -1 to T.
+                return (T)Convert.ChangeType(-1, typeof(T));
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            int iterator = 0;
+            while (iterator < size)
+            {
+                /*
+                 * Using yield to define an iterator removes the need for an explicit extra 
+                 * class (the class that holds the state for an enumeration) when you implement
+                 * the IEnumerable and IEnumerator pattern for a custom collection type. 
+                 * 
+                 * Each iteration of the foreach loop calls the iterator method. When a 
+                 * yield return statement is reached in the iterator method, expression is returned,
+                 * and the current location in code is retained. Execution is restarted from that
+                 * location the next time that the iterator function is called.
+                 */
+                yield return dy_arr[iterator++];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T element)
+        {
+            int index = Array.IndexOf(dy_arr, element);
+            return index;
+        }
+
+        public void Insert(int index, T item)
+        {
+            int iterator = size;
+            //Push every element before the index 1 block to its right to make room for the insertion
+            while (iterator > index)
+            {
+                dy_arr[iterator] = dy_arr[iterator - 1];
+                iterator--;
+            }
+            dy_arr[index] = item;
+        }
+
+        public void Print()
+        {
+            foreach (T element in dy_arr)
+            {
+                Console.Write(element + " ");
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index == -1)
+                return false;
+            //Push every element after the index 1 block to its left. This will overwrite the value
+            //to be deleted.
+            while (index < size)
+            {
+                dy_arr[index] = dy_arr[index + 1];
+                index++;
+            }
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            while (index < size)
+            {
+                dy_arr[index] = dy_arr[index + 1];
+                index++;
+            }
+        }
+
+    }
+}
---
+++ Assignment01/assignment1_partb_q1/assignment1_partb_q1/Program.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace assignment1_partb_q1
+{
+    class Program
+    {
+        //Part B Ques 3
+        static void Main(string[] args)
+        {
+            //Part B Ques 3 part a
+            CompareDouble();
+            //Part B Ques 3 part b
+            CompareDec();
+            //Part B Ques 3 part c
+            CompareBool();
+        }
+        
+        //The functions follow the same structure as the Main() method in Part A submission.
+        //Kindly refer to Main() method of part A to understand comments.
+
+        public static void CompareBool()
+        {
+            Console.WriteLine("\n------------For Boolean DataType----------\n");
+            Console.WriteLine("\n\n------------Searching through the values----------\n");
+
+            int size = 1000000;
+            int random_search_size = 5;
+            DynamicArray<bool> dy_arr = new DynamicArray<bool>(size);
+            List<bool> myList = new List<bool>(size);
+            bool[] myArray = new bool[size];
+            Random rnd = new Random();
+            int myNum;
+            bool myBool;
+
+            for (int index = 0; index < size; index++)
+            {
+                myNum = rnd.Next(1, 100);
+                if (myNum < 50)
+                    myBool = true;
+                else myBool = false;
+
+                dy_arr.Add(myBool);
+                myArray[index] = myBool;
+                myList.Add(myBool);
+            }
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            bool[] myRandom = new bool[random_search_size];
+            for (int index = 0; index < random_search_size; index++)
+            {
+                myNum = rnd.Next(1, 100);
+                if (myNum < 50)
+                    myBool = true;
+                else myBool = false;
+                myRandom[index] = myBool;
+            }
+            int hits = 0;
+            watch.Restart();
+            foreach (bool rand in myRandom)
+            {
+                if (Array.IndexOf(myArray, rand) != -1)
+                {
+                    hits++;
+                }
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("Array: " + elapsedMs + " Number of hits: " + hits);
+            hits = 0;
+            watch.Restart();
+            foreach (bool rand in myRandom)
+            {
+                if (myList.IndexOf(rand) != -1)
+                {
+                    hits++;
+                }
+            }
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("List: " + elapsedMs + " Number of hits: " + hits);
+            hits = 0;
+            watch.Restart();
+            foreach (bool rand in myRandom)
+            {
+                if (dy_arr.IndexOf(rand) != -1)
+                {
+                    hits++;
+                }
+            }
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("MyDynamicArrayList: " + elapsedMs + " Number of hits: " + hits+"\n");
+        }
+        public static void CompareDec()
+        {
+            Console.WriteLine("\n------------For Decimal DataType----------\n");
+            Console.WriteLine("\n------------Traversing the collections and summing the values----------\n");
+            int size = 1000000;
+            int random_search_size = 5;
+            DynamicArray<decimal> dy_arr = new DynamicArray<decimal>(size);
+            List<decimal> myList = new List<decimal>(size);
+            decimal[] myArray = new decimal[size];
+            Random rnd = new Random();
+            decimal myNum;
+
+            for (int index = 0; index < size; index++)
+            {
+                myNum = (decimal)rnd.NextDouble();
+                dy_arr.Add(myNum);
+                myArray[index] = myNum;
+                myList.Add(myNum);
+            }
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            decimal sum = 0;
+            foreach (decimal num in myArray)
+            {
+                sum += num;
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("Array Time: " + elapsedMs + " and Sum: " + sum);
+            watch.Restart();
+            sum = 0;
+            foreach (decimal num in myList)
+            {
+                sum += num;
+            }
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("List: Time: " + elapsedMs + " and Sum: " + sum);
+            watch.Restart();
+            sum = 0;
+            for (int index = 0; index < size; index++)
+            {
+                sum += dy_arr.Get(index);
+            }
+
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("MyDynamicArray: Time: " + elapsedMs + " and Sum: " + sum);
+
+            //Searching another set of 5 random numbers
+            decimal[] myRandom = new decimal[random_search_size];
+            for (int index = 0; index < random_search_size; index++)
+            {
+                myNum = (decimal)rnd.NextDouble();
+                myRandom[index] = myNum;
+            }
+            int hits = 0;
+            watch.Restart();
+            foreach (decimal rand in myRandom)
+            {
+                if (Array.IndexOf(myArray, rand) != -1)
+                {
+                    hits++;
+                }
+            }
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("\n\n------------Searching through the values----------\n");
+            Console.WriteLine("Array: " + elapsedMs + " Number of hits: " + hits);
+            hits = 0;
+            watch.Restart();
+            foreach (decimal rand in myRandom)
+            {
+                if (myList.IndexOf(rand) != -1)
+                {
+                    hits++;
+                }
+            }
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("List: " + elapsedMs + " Number of hits: " + hits);
+            hits = 0;
+            watch.Restart();
+            foreach (decimal rand in myRandom)
+            {
+                if (dy_arr.IndexOf(rand) != -1)
+                {
+                    hits++;
+                }
+            }
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("MyDynamicArrayList: " + elapsedMs + " Number of hits: " + hits);
+
+        }
+        public static void CompareDouble()
+        {
+            Console.WriteLine("\n------------For Double DataType----------\n");
+            Console.WriteLine("\n------------Traversing the collections and summing the values----------\n");
+            int size = 1000000;
+            int random_search_size = 5;
+            DynamicArray<double> dy_arr = new DynamicArray<double>(size);
+            List<double> myList = new List<double>(size);
+            double[] myArray = new double[size];
+            Random rnd = new Random();
+            double myNum;
+
+            for (int index = 0; index < size; index++)
+            {
+                myNum = rnd.NextDouble();
+                dy_arr.Add(myNum);
+                myArray[index] = myNum;
+                myList.Add(myNum);
+            }
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            double sum = 0;
+            foreach (double num in myArray)
+            {
+                sum += num;
+            }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("Array Time: " + elapsedMs + " and Sum: " + sum);
+            watch.Restart();
+            sum = 0;
+            foreach (double num in myList)
+            {
+                sum += num;
+            }
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("List Time: " + elapsedMs + " and Sum: " + sum);
+            watch.Restart();
+            sum = 0;
+            for (int index = 0; index < size; index++)
+            {
+                sum += dy_arr.Get(index);
+            }
+
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("MyDynamicArray Time: " + elapsedMs + " and Sum: " + sum);
+
+            double[] myRandom = new double[random_search_size];
+            for (int index = 0; index < random_search_size; index++)
+            {
+                myNum = rnd.NextDouble();
+                myRandom[index] = myNum;
+            }
+            int hits = 0;
+            watch.Restart();
+            foreach (double rand in myRandom)
+            {
+                if (Array.IndexOf(myArray, rand) != -1)
+                {
+                    hits++;
+                }
+            }
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("\n\n------------Searching through the values----------\n");
+
+            Console.WriteLine("Array: " + elapsedMs + " Number of hits: " + hits);
+            hits = 0;
+            watch.Restart();
+            foreach (double rand in myRandom)
+            {
+                if (myList.IndexOf(rand) != -1)
+                {
+                    hits++;
+                }
+            }
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("List: " + elapsedMs + " Number of hits: " + hits);
+            hits = 0;
+            watch.Restart();
+            foreach (double rand in myRandom)
+            {
+                if (dy_arr.IndexOf(rand) != -1)
+                {
+                    hits++;
+                }
+            }
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("MyDynamicArrayList: " + elapsedMs + " Number of hits: " + hits);
+        }
+    }
+}
